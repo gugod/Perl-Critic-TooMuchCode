b69f99d7da34cd42f386706d8db6d5d2df8ff802
Kang-min Liu <gugod@gugod.org>
2018-06-26 22:58:55 +0900

    doc (or not)

---
+++ lib/Perl/Critic/TooMuchCode.pm
@@ -0,0 +1,15 @@
+package Perl::Critic::TooMuchCode;
+
+1;
+__END__
+
+=head1 NAME
+
+Perl::Critic::TooMuchCode - perlcritic add-ons that generally check for un-needed stuffs.
+
+=head1 DESCRIPTION
+
+Identifying what can be removed is often tedious and sometimes non-trivial to do. Bots can
+probably do it better then humans.
+
+=cut
