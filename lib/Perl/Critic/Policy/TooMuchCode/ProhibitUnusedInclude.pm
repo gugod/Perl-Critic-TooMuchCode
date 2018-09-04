b8ea208fcebce6817887b794c2eea36f56b757d5
Kang-min Liu <gugod@gugod.org>
2018-09-04 17:50:26 +0900

    ProhibitUnusedInclude: try to handle generic case.
    Instead of having a whitelist of module names here, try gathering
    the usage of these 2 cases:
    
        use Foobar;
        Foobar::process(); # case 1
        Foobar->process(); # case 2
    
    In both cases, the module name "Foobar" is explicitly coded.
    
    However, the absent of module name in the code does not automacially
    implies that Foobar is unused.

--- lib/Perl/Critic/Policy/TooMuchCode/ProhibitUnusedInclude.pm
+++ lib/Perl/Critic/Policy/TooMuchCode/ProhibitUnusedInclude.pm
@@ -2,6 +2,7 @@ package Perl::Critic::Policy::TooMuchCode::ProhibitUnusedInclude;
 
 use strict;
 use warnings;
+use Scalar::Util qw(refaddr);
 use Perl::Critic::Utils;
 use parent 'Perl::Critic::Policy';
 
@@ -12,60 +13,83 @@ sub applies_to           { return 'PPI::Document' }
 
 sub violates {
     my ( $self, $elem, $doc ) = @_;
-    my @violations = $self->gather_violations_try_family($elem, $doc);
-    push @violations, $self->gather_violations_objective($elem, $doc);
-    return @violations;
+
+    my @includes = grep {
+        !$_->pragma && $_->module !~ /\A Mo([ou](?:se)?)? (\z|::)/x
+    } @{ $doc->find('PPI::Statement::Include') ||[] };
+    return () unless @includes;
+
+    my %uses;
+    $self->gather_uses_try_family(\@includes, $doc, \%uses);
+    $self->gather_uses_objective(\@includes, $doc, \%uses);
+    $self->gather_uses_generic(\@includes, $doc, \%uses);
+
+    return map {
+        $self->violation(
+            "Unused include: " . $_->module,
+            "A module is `use`-ed but not really consumed in other places in the code",
+            $_
+        )
+    } grep {
+        ! $uses{refaddr($_)}
+    } @includes;
 }
 
-sub gather_violations_try_family {
-    my ( $self, $elem, $doc ) = @_;
+sub gather_uses_generic {
+    my ( $self, $includes, $doc, $uses ) = @_;
 
-    my @violations;
+    my @words = grep { ! $_->statement->isa('PPI::Statement::Include') } @{ $doc->find('PPI::Token::Word') || []};
+    my @mods = map { $_->module } @$includes;
 
-    my @includes = @{ $elem->find('PPI::Statement::Include') ||[] };
-    return @violations unless @includes;
+    my %used;
+    for my $word (@words) {
+        for my $inc (@$includes) {
+            my $mod = $inc->module;
+            if ($word->content =~ /\A $mod (\z|::)/x) {
+                $uses->{ refaddr($inc) } = 1;
+            }
+        }
+    }
+}
+
+sub gather_uses_try_family {
+    my ( $self, $includes, $doc, $uses ) = @_;
+
+    my %is_try = map { $_ => 1 } qw(Try::Tiny Try::Catch Try::Lite TryCatch Try);
+    my @uses_tryish_modules = grep { $is_try{$_->module} } @$includes;
+    return unless @uses_tryish_modules;
 
     my $has_try_block = 0;
-    for my $try_keyword (@{ $elem->find(sub { $_[1]->isa('PPI::Token::Word') && $_[1]->content eq 'try' }) ||[]}) {
+    for my $try_keyword (@{ $doc->find(sub { $_[1]->isa('PPI::Token::Word') && $_[1]->content eq 'try' }) ||[]}) {
         my $try_block = $try_keyword->snext_sibling or next;
         next unless $try_block->isa('PPI::Structure::Block');
         $has_try_block = 1;
         last;
     }
+    return unless $has_try_block;
 
-    for my $try_module (qw(Try::Tiny Try::Catch Try::Lite TryCatch Try)) {
-        my @uses = grep { $_->module eq $try_module } @includes;
-        if (@uses && !$has_try_block) {
-            push @violations, map { $self->violation("Unused ${try_module} module", 'There are no `try` block in the code.', $_) } @uses;
-        }
-    }
-
-    return @violations;
+    $uses->{refaddr($_)} = 1 for @uses_tryish_modules;
 }
 
-sub gather_violations_objective {
-    my ( $self, $elem, $doc ) = @_;
+sub gather_uses_objective {
+    my ( $self, $includes, $doc, $uses ) = @_;
 
-    my @violations;
+    my %is_objective = map { ($_ => 1) } qw(HTTP::Tiny HTTP::Lite LWP::UserAgent File::Spec);
+    for my $inc (@$includes) {
+        my $mod = $inc->module;
+        next unless $is_objective{ $mod };
 
-    for my $class_name (qw(HTTP::Tiny HTTP::Lite LWP::UserAgent File::Spec)) {
-        my @include_statements = grep { $_->module eq $class_name } @{ $elem->find('PPI::Statement::Include') ||[] };
-        next unless @include_statements;
-
-        my $is_used = $elem->find(
+        my $is_used = $doc->find(
             sub {
                 my $el = $_[1];
-                $el->isa('PPI::Token::Word') && $el->content eq $class_name && !($el->parent->isa('PPI::Statement::Include'))
+                $el->isa('PPI::Token::Word') && $el->content eq $mod && !($el->parent->isa('PPI::Statement::Include'))
             }
         );
 
-        unless ($is_used) {
-            push @violations, map {
-                $self->violation("Unused ${class_name} module.", "No methods from ${class_name} are invoked.", $_)
-            } @include_statements;
+        if ($is_used) {
+            $uses->{ refaddr($inc) } = 1;
         }
     }
-    return @violations;
 }
 
 1;
