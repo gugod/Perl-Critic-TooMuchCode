037a000874cecec7a016fb084381aa38eef85e02
Kang-min Liu <gugod@gugod.org>
2019-12-17 08:31:23 +0900

    documentation and minor refactoring

--- lib/Perl/Critic/Policy/TooMuchCode/ProhibitUnusedImport.pm
+++ lib/Perl/Critic/Policy/TooMuchCode/ProhibitUnusedImport.pm
@@ -19,32 +19,9 @@ my %is_special = map { $_ => 1 } qw(Getopt::Long MooseX::Foreign MouseX::Foreign
 
 sub violates {
     my ( $self, $elem, $doc ) = @_;
-    my @violations = $self->gather_violations_generic($elem, $doc);
-    return @violations;
-}
-
-sub gather_violations_generic {
-    my ( $self, $elem, $doc ) = @_;
 
     my %imported;
-
-    my $include_statements = $elem->find(sub { $_[1]->isa('PPI::Statement::Include') && !$_[1]->pragma }) || [];
-    for my $st (@$include_statements) {
-        next if $st->schild(0) eq 'no';
-        my $expr_qw = $st->find( sub { $_[1]->isa('PPI::Token::QuoteLike::Words'); }) or next;
-
-        my $included_module = $st->schild(1);
-        next if $is_special{$included_module};
-
-        if (@$expr_qw == 1) {
-            my $expr = $expr_qw->[0];
-            my @words = $expr_qw->[0]->literal;
-            for my $w (@words) {
-                next if $w =~ /\A [:\-\+]/x;
-                push @{ $imported{$w} //=[] }, $included_module;
-            }
-        }
-    }
+    $self->gather_imports_generic( \%imported, $elem, $doc );
 
     my %used;
     for my $el_word (@{ $elem->find( sub { $_[1]->isa('PPI::Token::Word') }) ||[]}) {
@@ -59,7 +36,7 @@ sub gather_violations_generic {
     Perl::Critic::Policy::Variables::ProhibitUnusedVariables::_get_symbol_usage(\%used, $doc);
     Perl::Critic::Policy::Variables::ProhibitUnusedVariables::_get_regexp_symbol_usage(\%used, $doc);
 
-   my @violations;
+    my @violations;
     my @to_report = grep { !$used{$_} } (sort keys %imported);
     for my $tok (@to_report) {
         for my $inc_mod (@{ $imported{$tok} }) {
@@ -69,6 +46,29 @@ sub gather_violations_generic {
 
     return @violations;
 }
+
+sub gather_imports_generic {
+    my ( $self, $imported, $elem, $doc ) = @_;
+
+    my $include_statements = $elem->find(sub { $_[1]->isa('PPI::Statement::Include') && !$_[1]->pragma }) || [];
+    for my $st (@$include_statements) {
+        next if $st->schild(0) eq 'no';
+        my $expr_qw = $st->find( sub { $_[1]->isa('PPI::Token::QuoteLike::Words'); }) or next;
+
+        my $included_module = $st->schild(1);
+        next if $is_special{$included_module};
+
+        if (@$expr_qw == 1) {
+            my $expr = $expr_qw->[0];
+            my @words = $expr_qw->[0]->literal;
+            for my $w (@words) {
+                next if $w =~ /\A [:\-\+]/x;
+                push @{ $imported->{$w} //=[] }, $included_module;
+            }
+        }
+    }
+}
+
 
 1;
 
@@ -83,8 +83,17 @@ TooMuchCode::ProhibitUnusedImport -- Find unused imports
 An "Unused Import" is usually a subroutine name imported by a C<use> statement.
 For example, the word C<Dumper> in the following statement:
 
-    use Data::Dumper qw<Dumper>;
+    use Foo qw( baz );
 
-If the rest of program has not mentioned the word C<Dumper>, then it can be deleted.
+The word C<baz> can be removed if it is not mentioned in the rest of this program.
+
+Conventionally, this policy looks only for the C<use> statement with a C<qw()>
+operator at the end. This syntax is easier to deal with. It also works with
+the usaeg of C<Importer> module -- as long as a C<qw()> is there at the end:
+
+    use Importer 'Foo' => qw( baz );
+
+This may be adjusted to be a bit smarter, but it is a clear convention in the
+beginning.
 
 =cut
